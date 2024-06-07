9db8d3f3a24f1b4b8da7a9ddde332a30a3acf780
Fayshal007 <fayshalislam5@gmail.com>
2024-06-07 06:43:04 +0600

    search function implement

--- scripts/phones.js
+++ scripts/phones.js
@@ -1,19 +1,20 @@
 // all phone data load
-const loadPhoneData = async() => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhoneData = async(phoneName) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${phoneName}`);
     const data = await res.json();
     const phones = data.data;
+
     displayPhones(phones);
 }
 
 
 // all phone data show
 const displayPhones = phones =>{
-    const phonesContainer = document.getElementById('phones-container')
+    const phonesContainer = document.getElementById('phones-container');
+    phonesContainer.textContent = ''
     for (const phone of phones) {
-        console.log(phone);
         const div = document.createElement('div');
-        div.classList = "card w-80 bg-blue-50 shadow-xl shadow-blue-500/50 mx-auto"
+        div.classList = "card w-80 bg-blue-50 shadow-xl shadow-blue-500/50 mx-auto";
         div.innerHTML = `
             <figure class="px-5 pt-5">
                 <img src="${phone.image}" alt="Shoes" class="rounded-xl" />
@@ -29,5 +30,16 @@ const displayPhones = phones =>{
         phonesContainer.appendChild(div)
     }
 }
+
+
+//search phone
+document.getElementById('search-button').addEventListener('click', function(){
+    const searchField = document.getElementById('search-field');
+    const searchFieldValue = searchField.value;
+    console.log(searchFieldValue);
+    loadPhoneData(searchFieldValue);
+
+    searchField.value = ''
+})
 
 loadPhoneData()
